c329ac8ede15ce9defb789da3df36725b076de6c
Fahim-16 <fahim.sakeer@gmail.com>
2022-07-07 16:54:50 +0530

    Library app completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { SearchbookComponent } from './searchbook/searchbook.component';
+import { DeletebookComponent } from './deletebook/deletebook.component';
+import { ViewbookComponent } from './viewbook/viewbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddbookComponent,
+    SearchbookComponent,
+    DeletebookComponent,
+    ViewbookComponent
   ],
   imports: [
     BrowserModule,
